658cbe38c7cfe75b793e6e2192175e3eec233a70
OnurKaral <santobixua@gmail.com>
2020-12-21 18:39:38 +0300

    unnecessary imports

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,2 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _service = require('./lib/service');
-var _axios = require('axios'); var _axios2 = _interopRequireDefault(_axios);
-const ENDPOINT = "https://jsonplaceholder.typicode.com";
+"use strict";var _service = require('./lib/service');
 console.log(_service.getData.call(void 0, 1));
--- dist/lib/service.js
+++ dist/lib/service.js
@@ -1,7 +1,6 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _axios = require('axios'); var _axios2 = _interopRequireDefault(_axios);
 const ENDPOINT = "https://jsonplaceholder.typicode.com";
 async function getData(Number) {
-    
     const { data: users } = await _axios2.default.get(`${ENDPOINT}/users/${Number}`);
     console.log(users);
     const { data: posts } = await _axios2.default.get(`${ENDPOINT}/posts?userId=${Number}`);
